56f1a1fe38fbd35dc8d012c26e7fcb68da3902bd
Jonathan C. Warfield <jon.warfield@robotsandpencils.com>
2018-03-07 04:46:08 -0500

    web app index page

--- test/election.js
+++ test/election.js
@@ -2,6 +2,8 @@ var Election = artifacts.require("./Election.sol");
 
 contract("Election", function(accounts) {
 
+    var electionInstance;
+
     it("initializes with two candidates", function () {
         return Election.deployed().then(function(instance) {
             return instance.candidatesCount();
